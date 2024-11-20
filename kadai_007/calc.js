586089a4bf8edb6164cacdfa41f85c40ae41d649
kouji1014 <91065589+kouji1014@users.noreply.github.com>
2024-11-20 17:54:00 +0900

    kadai_007をプッシュ

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,10 +1,10 @@
-const num=3;
-if (num%3==0){
-        console.log("3の倍数です");
+const num=27;
+if (num%15==0){
+        console.log("3と5の倍数です");
 }else if(num%5==0){
     console.log("5の倍数です");
-}else if(num%5==0 && num%3==0){
-    console.log("5の倍数です");
+}else if(num%3==0){
+    console.log("3の倍数です");
 }else{
     console.log(num);
 }
